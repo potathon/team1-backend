e2c36258f19681546b15da8455ed401ac8d585e7
lky3004me <lky3004me@gmail.com>
2024-07-13 20:41:20 +0900

    :sparkles: bug changesCommit

--- src/main/java/com/potaton/potato/domain/daily/service/DailyServiceImpl.java
+++ src/main/java/com/potaton/potato/domain/daily/service/DailyServiceImpl.java
@@ -1,10 +1,13 @@
 package com.potaton.potato.domain.daily.service;
 
 import com.potaton.potato.domain.daily.dto.requestdto.DailyCompleteDto;
+import com.potaton.potato.domain.daily.dto.requestdto.ReplyDto;
 import com.potaton.potato.domain.daily.dto.responsedto.DailyInfoDto;
 import com.potaton.potato.domain.daily.dto.responsedto.DailyQuestionDto;
 import com.potaton.potato.domain.daily.dto.responsedto.DailyReviewDto;
 import com.potaton.potato.domain.daily.repository.*;
+import com.potaton.potato.domain.user.entity.User;
+import com.potaton.potato.domain.user.repository.UserJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,14 +22,16 @@ public class DailyServiceImpl implements DailyService {
     private final DailyQuestionJpaRepository dailyQuestionJpaRepository;
     private final QuestionJpaRepository questionJpaRepository;
     private final TagJpaRepository tagJpaRepository;
+    private final UserJpaRepository userJpaRepository;
 
     @Autowired
-    public DailyServiceImpl(AnswerJpaRepository answerJpaRepository, DailyJpaRepository dailyJpaRepository, DailyQuestionJpaRepository dailyQuestionJpaRepository, QuestionJpaRepository questionJpaRepository, TagJpaRepository tagJpaRepository) {
+    public DailyServiceImpl(AnswerJpaRepository answerJpaRepository, DailyJpaRepository dailyJpaRepository, DailyQuestionJpaRepository dailyQuestionJpaRepository, QuestionJpaRepository questionJpaRepository, TagJpaRepository tagJpaRepository, UserJpaRepository userJpaRepository) {
         this.answerJpaRepository = answerJpaRepository;
         this.dailyJpaRepository = dailyJpaRepository;
         this.dailyQuestionJpaRepository = dailyQuestionJpaRepository;
         this.questionJpaRepository = questionJpaRepository;
         this.tagJpaRepository = tagJpaRepository;
+        this.userJpaRepository = userJpaRepository;
     }
 
 
@@ -47,6 +52,10 @@ public class DailyServiceImpl implements DailyService {
 
     @Override
     public void completeDaily(DailyCompleteDto dailyCompleteDto) {
+        List<ReplyDto> replies = dailyCompleteDto.getReplies();
+        Long userId = dailyCompleteDto.getUserId();
+
+        User user = userJpaRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
 
     }
 

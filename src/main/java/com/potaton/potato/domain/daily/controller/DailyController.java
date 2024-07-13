0391db7eb4408354a49ced92c96493a2c5446628
lky3004me <lky3004me@gmail.com>
2024-07-13 19:58:22 +0900

    :sparkles: feat dailyController compelted

--- src/main/java/com/potaton/potato/domain/daily/controller/DailyController.java
+++ src/main/java/com/potaton/potato/domain/daily/controller/DailyController.java
@@ -1,12 +1,51 @@
 package com.potaton.potato.domain.daily.controller;
 
+import com.potaton.potato.domain.daily.dto.requestdto.DailyCompleteDto;
+import com.potaton.potato.domain.daily.dto.responsedto.DailyInfoDto;
+import com.potaton.potato.domain.daily.dto.responsedto.DailyQuestionDto;
+import com.potaton.potato.domain.daily.dto.responsedto.DailyReviewDto;
+import com.potaton.potato.domain.daily.service.DailyService;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 public class DailyController {
 
+    private DailyService dailyService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<DailyInfoDto>> getDailyList(){
+        List<DailyInfoDto> dailyList = dailyService.getDailyList();
+
+        return ResponseEntity.ok(dailyList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<DailyReviewDto>> getDailyReview(@PathVariable Long id){
+        List<DailyReviewDto> dailyReview = dailyService.getDailyReview(id);
+
+        return ResponseEntity.ok(dailyReview);
+    }
+
+    @GetMapping("/test/{id}")
+    public ResponseEntity<List<DailyQuestionDto>> getDailyQuestion(){
+        List<DailyQuestionDto> dailyQuestion = dailyService.getDailyQuestion();
+
+        return ResponseEntity.ok(dailyQuestion);
+    }
+
+    @PostMapping("/test/{id}")
+    public ResponseEntity<String> compeleteDaily(DailyCompleteDto dailyCompleteDto){
+        dailyService.completeDaily(dailyCompleteDto);
+
+        return ResponseEntity.ok("completeDaily success");
+    }
 }
--- src/main/java/com/potaton/potato/domain/user/controller/UserController.java
+++ src/main/java/com/potaton/potato/domain/user/controller/UserController.java
@@ -13,10 +13,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/login")
 public class UserController {
 
     private final UserService userService;

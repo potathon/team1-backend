e0dbeb8882157615c1bc03d633c19e9c20038c24
lky3004me <lky3004me@gmail.com>
2024-07-13 17:24:01 +0900

    feat : controller package

---
+++ src/main/java/com/potaton/potato/domain/daily/controller/DailyController.java
@@ -0,0 +1,12 @@
+package com.potaton.potato.domain.daily.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class DailyController {
+
+}
---
+++ src/main/java/com/potaton/potato/domain/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.potaton.potato.domain.user.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+public class UserController {
+
+}
